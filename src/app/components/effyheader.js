e0eac709f99198e141007f010cfbbc52157a125d
Effy Elden <effye@thoughtworks.com>
2018-01-14 22:59:22 +1100

    Change internal link in EffyHeader to use <Link> component for client-side routing.

--- src/app/components/effyheader.js
+++ src/app/components/effyheader.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom';
 
 class EffyHeader extends Component {
   render() {
     return (
         <div className="row" id="title">
             <h1 id="name">Effy Elden</h1>
-            <h4 id="identity"><a href="/">~ is{this.props.location.pathname.replace(/\//g, ' ')} ~</a></h4>
+            <h4 id="identity"><Link to="/">~ is{this.props.location.pathname.replace(/\//g, ' ')} ~</Link></h4>
         </div>
     )
   }
